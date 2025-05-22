dc313692404877c35391207ab8bc409cedcfc4d9
hwrd22 <113211442+hwrd22@users.noreply.github.com>
2025-05-22 13:00:12 -0700

    Reformatting commands to make it easier to pass to Minecraft

--- speech-recognition/speech-recog.py
+++ speech-recognition/speech-recog.py
@@ -52,75 +52,75 @@ def getResponse(response:str):
   # Finding strings in combined response and processing it.
   # Bee
   if "bee" in response:
-    allocateToArrays("spawn bees", "Spawning angry bees, buzz buzz buzz.", "bee", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn bee", "Spawning angry bees, buzz buzz buzz.", "bee", mcCmds, ttsVoiceClips, contains)
 
   # Wolf
   if "dog" in response:
-    allocateToArrays("spawn wolves", "Spawning angry wolves to steal your bones.", "dog", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn wolf", "Spawning angry wolves to steal your bones.", "dog", mcCmds, ttsVoiceClips, contains)
   elif "wolf" in response:  # Elif to avoid sending two TTS messages
-    allocateToArrays("spawn wolves", "Spawning angry wolves to steal your bones.", "wolf", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn wolf", "Spawning angry wolves to steal your bones.", "wolf", mcCmds, ttsVoiceClips, contains)
   
   # Polar Bear
   if "bear" in response:
-    allocateToArrays("spawn bears", "Spawning angry polar bears because you insulted their baby.", "bear", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn polar_bear", "Spawning angry polar bears because you insulted their baby.", "bear", mcCmds, ttsVoiceClips, contains)
 
   # Iron Golem
   if "iron" in response:
-    allocateToArrays("spawn golem", "Spawning an angry Iron Golem to yeet you in the air.", "iron", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn iron_golem", "Spawning an angry Iron Golem to yeet you in the air.", "iron", mcCmds, ttsVoiceClips, contains)
 
   # Zombie
   if "zombie" in response:
-    allocateToArrays("spawn zombies", "Spawning zombies to eat your brains.", "zombie", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn zombie", "Spawning zombies to eat your brains.", "zombie", mcCmds, ttsVoiceClips, contains)
   elif "rot" in response:
-    allocateToArrays("spawn zombies", "Spawning zombies to eat your brains.", "rot", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn zombie", "Spawning zombies to eat your brains.", "rot", mcCmds, ttsVoiceClips, contains)
 
   # Skeleton
   if "skeleton" in response:
-    allocateToArrays("spawn skeletons", "Spawning skeletons, welcome to the bone zone.", "skeleton", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn skeleton", "Spawning skeletons, welcome to the bone zone.", "skeleton", mcCmds, ttsVoiceClips, contains)
   elif "bone" in response:
-    allocateToArrays("spawn skeletons", "Spawning skeletons, welcome to the bone zone.", "bone", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn skeleton", "Spawning skeletons, welcome to the bone zone.", "bone", mcCmds, ttsVoiceClips, contains)
 
   # Creeper
   if "creeper" in response:
-    allocateToArrays("spawn creepers", "Spawning Creepers to blow up your house.", "creeper", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn creeper", "Spawning Creepers to blow up your house.", "creeper", mcCmds, ttsVoiceClips, contains)
   elif "cat" in response:  # Cat being associated with Creepers in some way, not rigged at all
-    allocateToArrays("spawn creepers", "Spawning Creepers to blow up your house.", "cat", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn creeper", "Spawning Creepers to blow up your house.", "cat", mcCmds, ttsVoiceClips, contains)
 
   # Spider
   if "spider" in response:
-    allocateToArrays("spawn spiders", "Spawning spiders to climb your walls.", "spider", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn spider", "Spawning spiders to climb your walls.", "spider", mcCmds, ttsVoiceClips, contains)
   elif "bug" in response:  # Generally the most common "bug" encountered in the game
-    allocateToArrays("spawn spiders", "Spawning spiders to climb your walls.", "bug", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn spider", "Spawning spiders to climb your walls.", "bug", mcCmds, ttsVoiceClips, contains)
 
   # Witch
   if "witch" in response:
-    allocateToArrays("spawn witches", "Spawning witches to cackle at you.", "witch", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn witch", "Spawning witches to cackle at you.", "witch", mcCmds, ttsVoiceClips, contains)
 
   # Silverfish
   if "silverfish" in response:
-    allocateToArrays("spawn silverfishes", "Spawning silverfish to eat your stones.", "silverfish", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn silverfish", "Spawning silverfish to eat your stones.", "silverfish", mcCmds, ttsVoiceClips, contains)
   elif "silver" in response:
-    allocateToArrays("spawn silverfishes", "Spawning silverfish to eat your stones.", "silver", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn silverfish", "Spawning silverfish to eat your stones.", "silver", mcCmds, ttsVoiceClips, contains)
   elif "fish" in response:
-    allocateToArrays("spawn silverfishes", "Spawning silverfish to eat your stones.", "fish", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn silverfish", "Spawning silverfish to eat your stones.", "fish", mcCmds, ttsVoiceClips, contains)
 
   # Phantom
   if "bed" in response:  # Relates to phantoms spawning if the player hasn't slept
-    allocateToArrays("spawn phantoms", "Spawning phantoms because you need some sleep.", "bed", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn phantom", "Spawning phantoms because you need some sleep.", "bed", mcCmds, ttsVoiceClips, contains)
   elif "sleep" in response:  # Ditto
-    allocateToArrays("spawn phantoms", "Spawning phantoms because you need some sleep.", "sleep", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn phantom", "Spawning phantoms because you need some sleep.", "sleep", mcCmds, ttsVoiceClips, contains)
   elif "phantom" in response:
-    allocateToArrays("spawn phantoms", "Spawning phantoms because you need some sleep.", "phantom", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn phantom", "Spawning phantoms because you need some sleep.", "phantom", mcCmds, ttsVoiceClips, contains)
 
   # Enderman
   if "end" in response:
-    allocateToArrays("spawn endermans", "Spawning angry Endermen to steal your blocks.", "end", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn enderman", "Spawning angry Endermen to steal your blocks.", "end", mcCmds, ttsVoiceClips, contains)
   
   # Illager (Will be Pillager in Minecraft)
   if "illager" in response:
-    allocateToArrays("spawn pillagers", "Spawning Illagers to steal your emeralds.", "illager", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn pillager", "Spawning Illagers to steal your emeralds.", "illager", mcCmds, ttsVoiceClips, contains)
   elif "raid" in response:  # Pillagers are associated with raids
-    allocateToArrays("spawn pillagers", "Spawning Illagers to steal your emeralds.", "raid", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn pillager", "Spawning Illagers to steal your emeralds.", "raid", mcCmds, ttsVoiceClips, contains)
   
   # Ravager
   if "ravager" in response:
@@ -128,73 +128,73 @@ def getResponse(response:str):
 
   # Slime
   if "slime" in response:
-    allocateToArrays("spawn slimes", "Spawning slimes to bounce on your head.", "slime", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn slime", "Spawning slimes to bounce on your head.", "slime", mcCmds, ttsVoiceClips, contains)
   
   # Breeze
   if "breeze" in response:
-    allocateToArrays("spawn breezes", "Spawning Breezes to blow you away.", "breeze", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn breeze", "Spawning Breezes to blow you away.", "breeze", mcCmds, ttsVoiceClips, contains)
   elif "wind" in response:
-    allocateToArrays("spawn breezes", "Spawning Breezes to blow you away.", "wind", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn breeze", "Spawning Breezes to blow you away.", "wind", mcCmds, ttsVoiceClips, contains)
   
   # Guardian
   if "guard" in response:
-    allocateToArrays("spawn guardians", "Spawning Guardians to go pew pew at you.", "guard", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn guardian", "Spawning Guardians to go pew pew at you.", "guard", mcCmds, ttsVoiceClips, contains)
   elif "sea" in response:  # Guardians are found in ocean monuments
-    allocateToArrays("spawn guardians", "Spawning Guardians to go pew pew at you.", "sea", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn guardian", "Spawning Guardians to go pew pew at you.", "sea", mcCmds, ttsVoiceClips, contains)
   elif "ocean" in response:  # Ditto
-    allocateToArrays("spawn guardians", "Spawning Guardians to go pew pew at you.", "ocean", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn guardian", "Spawning Guardians to go pew pew at you.", "ocean", mcCmds, ttsVoiceClips, contains)
 
   # Blaze
   if "blaze" in response:
-    allocateToArrays("spawn blazes", "Spawning Blazes to set you on fire.", "blaze", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn blaze", "Spawning Blazes to set you on fire.", "blaze", mcCmds, ttsVoiceClips, contains)
   elif "fire" in response:  # Blazes fire fireballs to players
-    allocateToArrays("spawn blazes", "Spawning Blazes to set you on fire.", "fire", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn blaze", "Spawning Blazes to set you on fire.", "fire", mcCmds, ttsVoiceClips, contains)
 
   # Piglin
   if "pig" in response:
-    allocateToArrays("spawn piglins", "Spawning angry Piglins to steal your gold.", "pig", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn piglin", "Spawning angry Piglins to steal your gold.", "pig", mcCmds, ttsVoiceClips, contains)
 
   # Piglin Brute
   if "brute" in response:
-    allocateToArrays("spawn brute", "Spawning a Piglin Brute to steal your gold with gold.", "brute", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn piglin_brute", "Spawning a Piglin Brute to steal your gold with gold.", "brute", mcCmds, ttsVoiceClips, contains)
   
   # Ghast
   if "ghast" in response:
-    allocateToArrays("spawn ghasts", "Spawning Ghasts to wail at you.", "ghast", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn ghast", "Spawning Ghasts to wail at you.", "ghast", mcCmds, ttsVoiceClips, contains)
   elif "ghost" in response:  # Ghasts are sometimes compared with ghosts/souls
-    allocateToArrays("spawn ghasts", "Spawning Ghasts to wail at you.", "ghost", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn ghast", "Spawning Ghasts to wail at you.", "ghost", mcCmds, ttsVoiceClips, contains)
   elif "soul" in response:  # Ditto
-    allocateToArrays("spawn ghasts", "Spawning Ghasts to wail at you.", "soul", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn ghast", "Spawning Ghasts to wail at you.", "soul", mcCmds, ttsVoiceClips, contains)
 
   # Wither Skeleton
   if "skull" in response:  # Wither skeletons drop Wither Skulls, an important item in Minecraft
-    allocateToArrays("spawn witherskeletons", "Spawning Wither Skeletons to not drop Wither Skulls.", "skull", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn wither_skeleton", "Spawning Wither Skeletons to not drop Wither Skulls.", "skull", mcCmds, ttsVoiceClips, contains)
   
   # Hoglin
   if "hog" in response:
-    allocateToArrays("spawn hoglins", "Spawning Hoglins to headbutt you.", "hog", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn hoglin", "Spawning Hoglins to headbutt you.", "hog", mcCmds, ttsVoiceClips, contains)
   
   # Magma Cube
   if "magma" in response:
-    allocateToArrays("spawn magmacubes", "Spawning Magma Cubes to bounce with fire.", "magma", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn magma_cube", "Spawning Magma Cubes to bounce with fire.", "magma", mcCmds, ttsVoiceClips, contains)
   elif "lava" in response:
-    allocateToArrays("spawn magmacubes", "Spawning Magma Cubes to bounce with fire.", "lava", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn magma_cube", "Spawning Magma Cubes to bounce with fire.", "lava", mcCmds, ttsVoiceClips, contains)
 
   # Endermite
   if "mite" in response:
-    allocateToArrays("spawn endermites", "Spawning Endermites because you forgot they existed.", "mite", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn endermite", "Spawning Endermites because you forgot they existed.", "mite", mcCmds, ttsVoiceClips, contains)
   elif "pearl" in response:  # Since they spawn from ender pearls
-    allocateToArrays("spawn endermites", "Spawning Endermites because you forgot they existed.", "pearl", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn endermite", "Spawning Endermites because you forgot they existed.", "pearl", mcCmds, ttsVoiceClips, contains)
 
   # Shulker
   if "shulker" in response:
-    allocateToArrays("spawn shulkers", "Spawning Shulkers to lift you up into the air.", "shulker", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn shulker", "Spawning Shulkers to lift you up into the air.", "shulker", mcCmds, ttsVoiceClips, contains)
   elif "chest" in response:  # Will be using all storage blocks for this mob
-    allocateToArrays("spawn shulkers", "Spawning Shulkers to lift you up into the air.", "chest", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn shulker", "Spawning Shulkers to lift you up into the air.", "chest", mcCmds, ttsVoiceClips, contains)
   elif "box" in response:  # It IS called a Shulker Box so
-    allocateToArrays("spawn shulkers", "Spawning Shulkers to lift you up into the air.", "box", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn shulker", "Spawning Shulkers to lift you up into the air.", "box", mcCmds, ttsVoiceClips, contains)
   elif "barrel" in response:  # A storage container in Minecraft
-    allocateToArrays("spawn shulkers", "Spawning Shulkers to lift you up into the air.", "barrel", mcCmds, ttsVoiceClips, contains)
+    allocateToArrays("spawn shulker", "Spawning Shulkers to lift you up into the air.", "barrel", mcCmds, ttsVoiceClips, contains)
 
   # Warden
   if "dark" in response:  # Applies the "Darkness" effect on players
